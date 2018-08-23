6728576c174ba14990811628b735f6232d917a5a
Debendra Oli <debendraoli@gmail.com>
2018-08-23 04:14:30 +0300

    removed dynalist api requirement

--- schema.sql
+++ schema.sql
@@ -6,7 +6,6 @@ CREATE TABLE users (
     password text not null,
     email text not null unique,
     tag text not null unique,
-    dynalist_api text not null,
     push_id text,
     push_email integer DEFAULT 1,
     push_web integer DEFAULT 1
