379ae8577ee5cba61734ba268c580d65446df2f4
ananuzement <anupokharelforevents@gmail.com>
2018-09-05 21:41:05 +0300

    Bug fixes and improvement. This closes #43, closes #42

--- notify/helper.py
+++ notify/helper.py
@@ -15,14 +15,14 @@ def save(data):
         old = open('old.txt', 'w', encoding='utf-8')
         for lines in data['nodes']:
             if lines['checked'] == False:
-                old.write(f"{lines['id']} || {lines['content']}\n")
+                old.write(f"{lines['id']} || {lines['content'].rstrip()}\n")
         old.close()
         exit()
     else:
         new = open('new.txt', 'w', encoding='utf-8')
         for lines in data['nodes']:
             if lines['checked'] == False:
-                new.write(f"{lines['id']} || {lines['content']}\n")
+                new.write(f"{lines['id']} || {lines['content'].rstrip()}\n")
         new.close()
         files = ['old.txt', 'new.txt']
 
@@ -41,19 +41,17 @@ def parse(old, new):
     '''
     Compare two files dynalist-a.txt (old) and dynalist-b.txt (new) that were previously saved before by save().
     '''
-    old_file = open(old, 'r', encoding='utf-8')
-    old_file = old_file.readlines()
-    new_file = open(new, 'r', encoding='utf-8')
-    new_file = new_file.readlines()
+    old_file = open(old, 'r', encoding='utf-8').readlines()
+    new_file = open(new, 'r', encoding='utf-8').readlines()
     diff = [line for line in new_file if line not in old_file]
     if diff:
         assigns = []
         mentions = []
         for line in diff:
             if line.count('@'):
-                mentions += re.findall('\s@([a-z.]{3,15})', line)
+                mentions += re.findall('\s@([a-z.]+)', line)
             elif line.count('#'):
-                assigns += re.findall('\s#([a-z.]{3,15})', line)
+                assigns += re.findall('\s#([a-z.]+)', line)
 
             if mentions:
                 for mention in mentions:
@@ -70,7 +68,7 @@ def parse(old, new):
                         split_content = line.split(' || ')
                         url = f"https://dynalist.io/d/{config['DYNALIST_FILE_ID']}#z={split_content[0]}"
                         sendmail('Dynalist Notifications: New Task', email,
-                                      f'Hi {assign},\n\nYou have been assigned a new task:\n\n"{split_content[1]}"\n{url}"\nGood luck :)')
+                                      f'Hi {assign},\n\nYou have been assigned a new task:\n\n"{split_content[1]}"\n{url}\nGood luck :)')
 
 
 def sendmail(subject, emailto, message): # Send email
