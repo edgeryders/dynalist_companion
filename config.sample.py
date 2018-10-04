c6ce219f27d1e152eae41c3e349fedda3f8cc85c
Matt <matthias@ansorgs.de>
2018-10-04 19:04:54 +0200

    fix: config file key DATABASE_PATH renamed

--- config.sample.py
+++ config.sample.py
@@ -2,17 +2,17 @@ import os
 
 APP_NAME = 'Edgeryders'
 PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
-DATABASE_PATH = os.path.join(PROJECT_PATH, 'resources', '')
+RESOURCES_PATH = os.path.join(PROJECT_PATH, 'resources', '')
 
 DEBUG = False
 PORT = 8080
 SMTP_HOST = 'smtp.gmail.com'
-SMTP_EMAIL = 'YOUR GMAIL Address'
-SMTP_PASSWORD = 'YOUR GMAIL PASSWORD'
+SMTP_EMAIL = 'YOUR EMAIL ADDRESS'
+SMTP_PASSWORD = 'YOUR EMAIL PASSWORD'
 SMTP_PORT = 587
 DYNALIST_URL = 'https://dynalist.io/api/v1/doc/read'
 DYNALIST_API_TOKEN = 'DYNALIST TOKEN'
 DYNALIST_FILE_ID = 'DYNALIST FILE ID'
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{DATABASE_PATH}database.db'
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{RESOURCES_PATH}database.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 GOOGLE_DRIVE_DIR_ID = ''
