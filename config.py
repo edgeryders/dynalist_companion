eeb79c524f9eeacaddc6496a32ae112383dc18c9
debendraoli <debendraoli@gmail.com>
2018-10-28 18:44:53 +0300

    Removed configuration variable and migrated to admin panel.

---
+++ config.py
@@ -0,0 +1,12 @@
+from os import path
+
+APP_NAME = 'Edgeryders'
+PROJECT_PATH = path.dirname(path.realpath(__file__))
+RESOURCES_PATH = path.join(PROJECT_PATH, 'resources', '')
+OLD_FILE = path.join(RESOURCES_PATH, 'old.txt')
+NEW_FILE = path.join(RESOURCES_PATH, 'new.txt')
+DYNALIST_BACKUP_DIR = path.join(RESOURCES_PATH, 'dynalist_backup')
+PORT = 8080
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{RESOURCES_PATH}database.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = '5d55ae0480ca689713d7148d6960fb34'
