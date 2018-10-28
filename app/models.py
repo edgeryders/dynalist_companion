ad9d4ef7877bad20595db887e312f55d030770b9
debendraoli <debendraoli@gmail.com>
2018-10-28 18:16:41 +0300

    Added admin panel model and migrateted code of fetching dynalist content from notify package.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from . import db, login_manager
 import re
 from datetime import datetime
-from vars import old_file
+from app import config
 from flask_login import UserMixin
 
 
@@ -16,15 +16,15 @@ class Users(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     browser_pushid = db.Column(db.Text)
-    push_email = db.Column(db.Integer, default='1')
-    push_web = db.Column(db.Integer, default='1')
+    push_email = db.Column(db.SmallInteger, default='1')
+    push_web = db.Column(db.SmallInteger, default='1')
     alert_deadline = db.Column(db.Integer, default='1')
     is_admin = db.Column(db.SmallInteger, default='0')
 
 
 def deadlines(username):
     try:
-        read_file = open(old_file, 'r', encoding='utf-8').read()
+        read_file = open(config['OLD_FILE'], 'r', encoding='utf-8').read()
         dates = re.findall('.*(20[0-9]{2}-\d{2}-\d{2}).*\.\s.*#%s' % username, read_file)
         dates = [datetime.strptime(date, '%Y-%m-%d') for date in dates]
         now = datetime.now()
@@ -44,10 +44,43 @@ def deadlines(username):
 
 class AppSettings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    app_name = db.Column(db.String(), default='Dynalist Companion')
-    backup_enabled = db.Column(db.SmallInteger, default='0')
-    backup_type = db.Column(db.SmallInteger, default='1')  # 1 = drive, 2 = google drive
-    google_drive_id = db.Column(db.String(), nullable=True)
-    backup_interval = db.Column(db.Integer, default='24')  # interval in hours
-    email_push_enabled = db.Column(db.SmallInteger, default='1')  # 1 = enabled, 0 = disabled
-    web_push_enabled = db.Column(db.SmallInteger, default='1')  # 1 = enabled, 0 = disabled
+    backup_enabled = db.Column(db.SmallInteger)
+    backup_type = db.Column(db.SmallInteger)  # 1 = drive, 2 = google drive
+    google_drive_id = db.Column(db.String())
+    backup_file_prefix = db.Column(db.String())
+    email_push_enabled = db.Column(db.SmallInteger)  # 1 = enabled, 0 = disabled
+    web_push_enabled = db.Column(db.SmallInteger)  # 1 = enabled, 0 = disabled
+    dynalist_api_token = db.Column(db.String())
+    dynalist_api_url = db.Column(db.String())
+    dynalist_api_file_id = db.Column(db.String())
+    smtp_host = db.Column(db.String())
+    smtp_port = db.Column(db.Integer)
+    smtp_email = db.Column(db.String())
+    smtp_password = db.Column(db.String())
+    secret_code = db.Column(db.String())
+
+
+def get_dynalist_data():
+    from urllib import request
+    from json import dumps, loads
+    from app import app_sett
+
+    body = {
+        'token': app_sett.dynalist_api_token,
+        'file_id': app_sett.dynalist_api_file_id
+    }
+    params = dumps(body).encode('utf-8')
+    headers = {'Content-Type': 'application/json', 'User-Agent': 'Debendra Bot'}
+    try:
+        req = request.Request(app_sett.dynalist_api_url, data=params, headers=headers)
+        resp = request.urlopen(req)
+        raw_data = resp.read().decode('utf-8')
+    except Exception as e:
+        return e
+    finally:
+        load_data = loads(raw_data)
+        if load_data['_code'] == 'Ok':
+            return load_data
+        else:
+            return load_data['_msg']
+
