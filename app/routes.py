a988c77642aa983cbc8aaa259ddbd8cc301a2ab0
debendraoli <debendraoli@gmail.com>
2018-10-28 18:18:25 +0300

    Added admin panel route, 404 page routing, better readbility, removed redunant multiple if statment and code.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, flash, abort
-from . import app, db
-from . models import Users, deadlines, AppSettings
+from . import app, db, app_sett
+from . models import Users, deadlines
 from . forms import RegistrationForm, LoginForm, SettingsForm, AppSettingsForm
 from flask_login import login_required, login_user, current_user, logout_user
 import hashlib
@@ -48,9 +48,9 @@ def settings():
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.email = form.email.data
-        current_user.alert_deadline = form.alert_deadline.data if form.alert_deadline.data else '1'
-        current_user.push_email = '1' if form.push_email.data else '0'
-        current_user.push_web = '1' if form.push_web.data else '0'
+        current_user.alert_deadline = form.alert_deadline.data
+        current_user.push_email = form.push_email.data
+        current_user.push_web = form.push_web.data
         db.session.commit()
         flash('Settings updated', 'success')
     form.username.data = current_user.username
@@ -61,12 +61,43 @@ def settings():
     return render_template('settings.html', form=form, title='Settings')
 
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 @login_required
 def admin():
-    if current_user.is_authenticated and current_user.admin:
+    if current_user.is_authenticated and current_user.is_admin:
         form = AppSettingsForm()
-        render_template('admin.html', form=form)
+        if form.validate_on_submit():
+            app_sett.backup_enabled = form.backup_enabled.data
+            app_sett.backup_type = form.backup_type.data
+            app_sett.google_drive_id = form.google_drive_id.data
+            app_sett.backup_file_prefix = form.backup_file_prefix.data
+            app_sett.email_push_enabled = form.email_push_enabled.data
+            app_sett.web_push_enabled = form.web_push_enabled.data
+            app_sett.dynalist_api_url = form.dynalist_api_url.data
+            app_sett.dynalist_api_token = form.dynalist_api_token.data
+            app_sett.dynalist_api_file_id = form.dynalist_api_file_id.data
+            app_sett.smtp_host = form.smtp_host.data
+            app_sett.smtp_port = form.smtp_port.data
+            app_sett.smtp_email = form.smtp_email.data
+            app_sett.smtp_password = form.smtp_password.data
+            app_sett.secret_code = form.secret_code.data
+            db.session.commit()
+            flash('Settings applied.', 'success')
+        form.backup_enabled.data = app_sett.backup_enabled
+        form.backup_type.data = app_sett.backup_type
+        form.google_drive_id.data = app_sett.google_drive_id
+        form.backup_file_prefix.data = app_sett.backup_file_prefix
+        form.email_push_enabled.data = app_sett.email_push_enabled
+        form.web_push_enabled.data = app_sett.web_push_enabled
+        form.dynalist_api_url.data = app_sett.dynalist_api_url
+        form.dynalist_api_token.data = app_sett.dynalist_api_token
+        form.dynalist_api_file_id.data = app_sett.dynalist_api_file_id
+        form.smtp_host.data = app_sett.smtp_host
+        form.smtp_port.data = app_sett.smtp_port
+        form.smtp_email.data = app_sett.smtp_email
+        form.smtp_password.data = app_sett.smtp_password
+        form.secret_code.data = app_sett.secret_code
+        return render_template('admin.html', title='Admin panel', form=form)
     abort(404)
 
 
