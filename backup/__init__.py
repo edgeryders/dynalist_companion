6f50b174e96c62cad61212f6f77af0359fc47ff5
debendraoli <debendraoli@gmail.com>
2018-10-31 22:04:55 +0300

    Bufixes: Backup enabled not implemented, improvements.

--- backup/__init__.py
+++ backup/__init__.py
@@ -19,18 +19,23 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
-data = get_dynalist_data()
 
-if type(data) is dict:
-    file_name = f'{app_sett.backup_file_prefix}.{datetime.utcnow().date()}.T{datetime.utcnow().hour}'\
-                f'{datetime.utcnow().minute}Z.json'  # file name: Edgeryders.DynalistTasks.2019.01.01.T0000Z.json
-    backup_file = path.join(config['DYNALIST_BACKUP_DIR'], file_name)
-    with open(backup_file, 'w') as f:
-        dump(data['nodes'], f)
-    if app_sett.backup_type == 2:
-        google_drive_upload(backup_file)
-        remove(backup_file)
-        logging.info('Backup file deleted from disk.')
-else:
-    logging.critical(data)
-    logging.critical('Exiting...')
+def run():
+    if not app_sett.backup_enabled:
+        logger.warning('Backup task initialized but disabled in settings.')
+        logger.warning('Exiting')
+        exit()
+    data = get_dynalist_data()
+    if type(data) is dict:
+        file_name = f'{app_sett.backup_file_prefix}.{datetime.utcnow().date()}.T{datetime.utcnow().hour}'\
+                    f'{datetime.utcnow().minute}Z.json'  # file name: Edgeryders.DynalistTasks.2019.01.01.T0000Z.json
+        backup_file = path.join(config['DYNALIST_BACKUP_DIR'], file_name)
+        with open(backup_file, 'w') as f:
+            dump(data['nodes'], f)
+        if app_sett.backup_type == 2:
+            google_drive_upload(backup_file)
+            remove(backup_file)
+            logging.info('Backup file deleted from disk.')
+    else:
+        logging.critical(data)
+        logging.critical('Exiting...')
