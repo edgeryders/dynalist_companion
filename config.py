d57fa9359b2350152f62cbefc5caa7f84232303a
debendraoli <debendraoli@gmail.com>
2018-10-31 17:25:30 +0300

    Removed port config variable in favor of cli tools.

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ RESOURCES_PATH = path.join(PROJECT_PATH, 'resources', '')
 OLD_FILE = path.join(RESOURCES_PATH, 'old.txt')
 NEW_FILE = path.join(RESOURCES_PATH, 'new.txt')
 DYNALIST_BACKUP_DIR = path.join(RESOURCES_PATH, 'dynalist_backup')
-PORT = 8080
 SQLALCHEMY_DATABASE_URI = f'sqlite:///{RESOURCES_PATH}database.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
