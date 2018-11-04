803a4ea55197e57270dae765e1d2e140f0b364a0
Matt <matthias@ansorgs.de>
2018-11-04 21:51:28 +0100

    form wording improvements

--- app/forms.py
+++ app/forms.py
@@ -13,7 +13,7 @@ class RegistrationForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired()], render_kw={"placeholder": "Email"})
     password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
-                                                                     EqualTo('password', 'Password do not match.')],
+                                                                     EqualTo('password', 'Passwords do not match.')],
                                      render_kw={"placeholder": "Confirm Password"})
     secret_code = StringField('Secret Code', validators=[DataRequired()],
                               render_kw={"placeholder":"Secret Code"})
@@ -38,7 +38,7 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()],
                            render_kw={"placeholder":"Username", "autofocus": True})
-    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder":"Password"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
     remember = BooleanField('Remember Login')
     submit = SubmitField('Login')
 
@@ -55,30 +55,30 @@ class SettingsForm(FlaskForm):
         if username.data != current_user.username:
             user = Users.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('Username already exists. Please choose different one.')
+                raise ValidationError('Username already exists. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = Users.query.filter_by(username=email.data).first()
             if user:
-                raise ValidationError('Email address already exists. Please choose different one.')
+                raise ValidationError('Email address already exists. Please choose a different one.')
 
 
 class AppSettingsForm(FlaskForm):
     backup_enabled = BooleanField('Enable Backup')
     backup_type = SelectField('Backup Type', choices=[('0', 'Backup Type'), ('1', 'Drive'), ('2', 'Google Drive')])
-    google_drive_id = StringField('Google Drive id')
-    backup_file_prefix = StringField('Backup file name prefix')
+    google_drive_id = StringField('Google Drive Folder ID')
+    backup_file_prefix = StringField('Backup Filename Prefix')
     email_push_enabled = BooleanField('Enable Email Push')
     web_push_enabled = BooleanField('Enable Web Push')
-    dynalist_api_token = StringField('Dynalist Api token')
-    dynalist_api_url = StringField('Dynalist Api url')
-    dynalist_api_file_id = StringField('Dynalist File id')
+    dynalist_api_token = StringField('Dynalist API token')
+    dynalist_api_url = StringField('Dynalist API URL')
+    dynalist_api_file_id = StringField('Dynalist File ID')
     smtp_host = StringField('SMTP Host')
     smtp_port = StringField('SMTP Port')
-    smtp_email = StringField('SMTP Email address')
+    smtp_email = StringField('SMTP Email Address')
     smtp_password = StringField('SMTP Password')
     secret_code = StringField('Secret Code', validators=[Length(min=8,
-                                                                message='Secret code must be greater than 8 in length.')])
+                                                                message='Secret code must be 8 characters or longer.')])
     submit = SubmitField('Save')
 
